32bef79461044db4ea7131c4656871eb9720ed5b
Your Name <you@example.com>
2025-07-05 20:29:48 +0530

    navbar,popular

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+
+// Swiper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const TitleCards = () => {
+  return (
+    <div className="titlecards">
+      <h2>Popular on Netflix</h2>
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        slidesPerView={5}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          480: { slidesPerView: 3 },
+          768: { slidesPerView: 4 },
+          1024: { slidesPerView: 5 },
+        }}
+      >
+        {cards_data.map((card, index) => (
+          <SwiperSlide key={index}>
+            <div className="card">
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default TitleCards;
