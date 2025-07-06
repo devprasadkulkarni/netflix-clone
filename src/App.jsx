77b8c8f700795bebbeb12b6aff72e4d5fe8be5ee
Your Name <you@example.com>
2025-07-06 13:56:52 +0530

    sign-in/sign-up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./components/pages/Home/Home";
+import Login from "./components/pages/Login/Login";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
