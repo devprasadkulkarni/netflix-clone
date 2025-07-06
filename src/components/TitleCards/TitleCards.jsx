de3bccb321988ce4527c2648da2cf9708a531c94
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-07-06 15:44:41 +0530

    homepage-tmdb-api-implemented

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
@@ -10,6 +10,29 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDRkZWEwNzAyNTc0YTBkNDY3NTRjYTY1MWYxOTEwMCIsIm5iZiI6MTc1MTc5MzA5My4xMjIsInN1YiI6IjY4NmEzZGM1ZmM3ZDg1NmJiNmE5NTk5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i2r0H3pUE_l8GnxkNXlpt5ypdEr1S2X5nJ0Epve2yIE",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="titlecards">
       <h2> {title ? title : "Popular on Netflix"} </h2>
@@ -26,11 +49,14 @@ const TitleCards = ({ title, category }) => {
           1024: { slidesPerView: 5 },
         }}
       >
-        {cards_data.map((card, index) => (
+        {apiData.map((card, index) => (
           <SwiperSlide key={index}>
             <div className="card">
-              <img src={card.image} alt={card.name} />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt={card.original_title}
+              />
+              <p>{card.original_title}</p>
             </div>
           </SwiperSlide>
         ))}
